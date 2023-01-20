75f8e9dc484273527dd8f2d9271a845a4dd8adbf
SUMANHN19 <117834018+SUMANHN19@users.noreply.github.com>
2023-01-20 18:25:13 +0530

    Add files via upload

---
+++ Trees.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './snap.css'
+export default function Trees() {
+    return (
+        <div id='displaydiv'>
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+            <img id='animalsimages' src="tree.jpg" class="rounded float" alt="..." />
+
+        </div>
+    )
+}
